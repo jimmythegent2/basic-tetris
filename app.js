ae00e937c14e977b55642c1ef5e9eca5b8cbf56f
myusername <johanthangchambers@gmail.com>
2020-05-24 21:41:13 -0400

    test first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMCOntentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const ScoreDisplay = document.querySelector('#score')
+  const StartBtn = ducment.querySelector('#start-button')
+  const width = 10
+
+  //The Tetrominoes
+  const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, witdth*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
+
+
+})
